d72110ee848bcd1ba5b7695094bc1a83e69dacc9
Maros Kollar <maros@k-1.com>
2015-10-28 21:00:44 +0100

    Cancelable events, ensure only one event running, naming

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ function EventWatcher (id, controller) {
     // Call superconstructor first (AutomationModule)
     EventWatcher.super_.call(this, id, controller);
     
-    this.callbackEvent = undefined;
+    this.eventCallback  = undefined;
+    this.callbackCancel = undefined;
+    this.timeout        = undefined;
 }
 
 inherits(EventWatcher, AutomationModule);
@@ -30,21 +32,32 @@ EventWatcher.prototype.init = function (config) {
 
     var self = this;
     
-    self.callbackEvent = _.bind(self.callbackEvent,self);
+    self.callbackEvent  = _.bind(self.handleEvent,self);
+    self.callbackCancel = _.bind(self.handleCancel,self);
     
+    _.each(self.config.eventCancel,function(event) {
+        self.controller.on(event, self.callbackCancel);
+    });
     _.each(self.config.event,function(event) {
         self.controller.on(event, self.callbackEvent);
     });
+    
 };
 
 EventWatcher.prototype.stop = function () {
     var self = this;
     
+    _.each(self.config.eventCancel,function(event) {
+        self.controller.off(event, self.eventCancel);
+    });
     _.each(self.config.event,function(event) {
         self.controller.off(event, self.callbackEvent);
     });
     
-    self.callbackEvent = undefined;
+    self.handleCancel();
+    
+    self.callbackEvent  = undefined;
+    self.callbackCancel = undefined;
     
     EventWatcher.super_.prototype.stop.call(this);
 };
@@ -53,15 +66,28 @@ EventWatcher.prototype.stop = function () {
 // --- Module methods
 // ----------------------------------------------------------------------------
 
-EventWatcher.prototype.callbackEvent = function() {
+EventWatcher.prototype.handleCancel = function() {
     var self = this;
     
-    console.log('[EventWatcher] Got event');
-    
-    self.runAction(0);
+    if (typeof(action) !== 'undefined') {
+        console.log('[EventWatcher] Got cancel event');
+        clearTimeout(self.timeout);
+        self.timeout = undefined;
+    };
 };
 
-EventWatcher.prototype.runAction = function(index) {
+EventWatcher.prototype.handleEvent = function() {
+    var self = this;
+    
+    if (typeof(self.timeout) === 'undefined') {
+        console.log('[EventWatcher] Got event');
+        self.processAction(0);
+    } else {
+        console.log('[EventWatcher] Ignoring event. Already running');
+    }
+};
+
+EventWatcher.prototype.processAction = function(index) {
     var self = this;
     
     var action = self.config.actions[index];
@@ -71,7 +97,7 @@ EventWatcher.prototype.runAction = function(index) {
     
     if (typeof(action.delay) === 'integer'
         && action.delay > 0) {
-        setTimeout(
+        self.timeout = setTimeout(
             _.bind(self.performAction,self,index),
             (action.delay * 1000)
         );
@@ -83,18 +109,27 @@ EventWatcher.prototype.runAction = function(index) {
 EventWatcher.prototype.performAction = function(index) {
     console.log('[EventWatcher] Running action index '+index);
     
+    // Always reset timeout
+    self.timeout = undefined;
+    
     _.each(action.switches,function(element) {
         var deviceObject = self.controller.devices.get(element.device);
         if (typeof(deviceObject) !== 'undefined') {
-            deviceObject.performCommand(element.status);
+            if (element.status === 'toggle') {
+                var level = deviceObject.get('metrics:level');
+                level = (level === 'on') ? 'off':'on'; 
+                deviceObject.performCommand(level);
+            } else {
+                deviceObject.performCommand(element.status);
+            }
         }
     });
     
-    _.each(action.dimmers,function(element) {
+    _.each(action.multilevels,function(element) {
         var deviceObject = self.controller.devices.get(element.device);
-        var status = parseInt(status);
+        var level = parseInt(level);
         if (typeof(deviceObject) !== 'undefined') {
-            deviceObject.performCommand('exact',status);
+            deviceObject.performCommand('exact',{ level: level });
         }
     });
     
@@ -113,5 +148,5 @@ EventWatcher.prototype.performAction = function(index) {
         }
     }
     
-    self.runAction(index+1);
+    self.processAction(index+1);
 };
