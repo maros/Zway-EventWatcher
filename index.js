82eb46622fc02cf60c4a498d4473601f635597ce
Maros Kollar <maros@k-1.com>
2015-10-28 14:45:29 +0100

    EventWatcher WIP

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ Description:
 function EventWatcher (id, controller) {
     // Call superconstructor first (AutomationModule)
     EventWatcher.super_.call(this, id, controller);
+    
+    this.callbackEvent = undefined;
 }
 
 inherits(EventWatcher, AutomationModule);
@@ -27,16 +29,22 @@ EventWatcher.prototype.init = function (config) {
     EventWatcher.super_.prototype.init.call(this, config);
 
     var self = this;
-
+    
+    self.callbackEvent = _.bind(self.callbackEvent,self);
+    
+    _.each(self.config.event,function(event) {
+        self.controller.on(event, self.callbackEvent);
+    });
 };
 
 EventWatcher.prototype.stop = function () {
     var self = this;
     
-    if (self.vDev) {
-        self.controller.devices.remove(self.vDev.id);
-        self.vDev = undefined;
-    }
+    _.each(self.config.event,function(event) {
+        self.controller.off(event, self.callbackEvent);
+    });
+    
+    self.callbackEvent = undefined;
     
     EventWatcher.super_.prototype.stop.call(this);
 };
@@ -45,3 +53,65 @@ EventWatcher.prototype.stop = function () {
 // --- Module methods
 // ----------------------------------------------------------------------------
 
+EventWatcher.prototype.callbackEvent = function() {
+    var self = this;
+    
+    console.log('[EventWatcher] Got event');
+    
+    self.runAction(0);
+};
+
+EventWatcher.prototype.runAction = function(index) {
+    var self = this;
+    
+    var action = self.config.actions[index];
+    if (typeof(action) === 'undefined') {
+        return;
+    }
+    
+    if (typeof(action.delay) === 'integer'
+        && action.delay > 0) {
+        setTimeout(
+            _.bind(self.performAction,self,index),
+            (action.delay * 1000)
+        );
+    } else {
+        self.performAction(index);
+    }
+};
+
+EventWatcher.prototype.performAction = function(index) {
+    console.log('[EventWatcher] Running action index '+index);
+    
+    _.each(action.switches,function(element) {
+        var deviceObject = self.controller.devices.get(element.device);
+        if (typeof(deviceObject) !== 'undefined') {
+            deviceObject.performCommand(element.status);
+        }
+    });
+    
+    _.each(action.dimmers,function(element) {
+        var deviceObject = self.controller.devices.get(element.device);
+        var status = parseInt(status);
+        if (typeof(deviceObject) !== 'undefined') {
+            deviceObject.performCommand('exact',status);
+        }
+    });
+    
+    _.each(action.scenes,function(element) {
+        var deviceObject = self.controller.devices.get(element);
+        if (typeof(deviceObject) !== 'undefined') {
+            deviceObject.performCommand('on');
+        }
+    });
+    
+    if (typeof(action.code) !== 'undefined') {
+        try {
+            eval(action.code)
+        } catch(e) {
+            console.error('[EventWatcher] Error running custom code in index '+index+': '+e)
+        }
+    }
+    
+    self.runAction(index+1);
+};
